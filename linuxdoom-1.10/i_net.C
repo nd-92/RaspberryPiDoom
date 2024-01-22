7b1d1a20f078e376b09583f6db3e0fc7834c3cf4
nd92 <nd203@leicester.ac.uk>
2024-01-22 05:25:05 +0000

    i_net.C compile without warning

--- linuxdoom-1.10/i_net.C
+++ linuxdoom-1.10/i_net.C
@@ -48,18 +48,11 @@ static const char
 #include "i_net.H"
 
 // For some odd reason...
-#define ntohl(x)                                                          \
-    ((unsigned long int)((((unsigned long int)(x) & 0x000000ffU) << 24) | \
-                         (((unsigned long int)(x) & 0x0000ff00U) << 8) |  \
-                         (((unsigned long int)(x) & 0x00ff0000U) >> 8) |  \
-                         (((unsigned long int)(x) & 0xff000000U) >> 24)))
+// #define ntohl(x) ((unsigned long int)((((unsigned long int)(x) & 0x000000ffU) << 24) | (((unsigned long int)(x) & 0x0000ff00U) << 8) | (((unsigned long int)(x) & 0x00ff0000U) >> 8) | (((unsigned long int)(x) & 0xff000000U) >> 24)))
+// #define ntohs(x) ((unsigned short int)((((unsigned short int)(x) & 0x00ff) << 8) | (((unsigned short int)(x) & 0xff00) >> 8)))
 
-#define ntohs(x)                                                      \
-    ((unsigned short int)((((unsigned short int)(x) & 0x00ff) << 8) | \
-                          (((unsigned short int)(x) & 0xff00) >> 8)))
-
-#define htonl(x) ntohl(x)
-#define htons(x) ntohs(x)
+// #define htonl(x) ntohl(x)
+// #define htons(x) ntohs(x)
 
 void NetSend(void);
 boolean NetListen(void);
@@ -68,7 +61,8 @@ boolean NetListen(void);
 // NETWORKING
 //
 
-int DOOMPORT = (IPPORT_USERRESERVED + 0x1d);
+// int DOOMPORT = (IPPORT_USERRESERVED + 0x1d);
+short unsigned int DOOMPORT = (IPPORT_USERRESERVED + 0x1d);
 
 int sendsocket;
 int insocket;
@@ -97,7 +91,7 @@ int UDPsocket(void)
 // BindToLocalPort
 //
 void BindToLocalPort(int s,
-                     int port)
+                     in_port_t port)
 {
     int v;
     struct sockaddr_in address;
@@ -148,18 +142,20 @@ void PacketSend(void)
 //
 void PacketGet(void)
 {
-    int i;
-    int c;
+    short i;
+    // size_t c;
     struct sockaddr_in fromaddress;
     int fromlen;
     doomdata_t sw;
 
     fromlen = sizeof(fromaddress);
-    c = recvfrom(insocket, &sw, sizeof(sw), 0, (struct sockaddr *)&fromaddress, (socklen_t *)(&fromlen));
+    ssize_t c = recvfrom(insocket, &sw, sizeof(sw), 0, (struct sockaddr *)&fromaddress, (socklen_t *)(&fromlen));
     if (c == -1)
     {
         if (errno != EWOULDBLOCK)
+        {
             I_Error("GetPacket: %s", strerror(errno));
+        }
         doomcom->remotenode = -1; // no packet
         return;
     }
@@ -167,14 +163,20 @@ void PacketGet(void)
     {
         static int first = 1;
         if (first)
+        {
             printf("len=%d:p=[0x%x 0x%x] \n", c, *(int *)&sw, *((int *)&sw + 1));
+        }
         first = 0;
     }
 
     // find remote node number
     for (i = 0; i < doomcom->numnodes; i++)
+    {
         if (fromaddress.sin_addr.s_addr == sendaddress[i].sin_addr.s_addr)
+        {
             break;
+        }
+    }
 
     if (i == doomcom->numnodes)
     {
@@ -184,7 +186,7 @@ void PacketGet(void)
     }
 
     doomcom->remotenode = i; // good packet from a game player
-    doomcom->datalength = c;
+    doomcom->datalength = static_cast<size_t>(c);
 
     // byte swap
     netbuffer->checksum = ntohl(sw.checksum);
@@ -204,7 +206,8 @@ void PacketGet(void)
     }
 }
 
-int GetLocalAddress(void)
+// int GetLocalAddress(void)
+char *GetLocalAddress(void)
 {
     char hostname[1024];
     struct hostent *hostentry; // host information entry
@@ -213,13 +216,18 @@ int GetLocalAddress(void)
     // get local address
     v = gethostname(hostname, sizeof(hostname));
     if (v == -1)
+    {
         I_Error("GetLocalAddress : gethostname: errno %d", errno);
+    }
 
     hostentry = gethostbyname(hostname);
     if (!hostentry)
+    {
         I_Error("GetLocalAddress : gethostbyname: couldn't get local host");
+    }
 
-    return *(int *)hostentry->h_addr_list[0];
+    // return *(int *)hostentry->h_addr_list[0];
+    return hostentry->h_addr_list[0];
 }
 
 //
@@ -241,22 +249,33 @@ void I_InitNetwork(void)
     {
         doomcom->ticdup = myargv[i + 1][0] - '0';
         if (doomcom->ticdup < 1)
+        {
             doomcom->ticdup = 1;
+        }
         if (doomcom->ticdup > 9)
+        {
             doomcom->ticdup = 9;
+        }
     }
     else
+    {
         doomcom->ticdup = 1;
+    }
 
     if (M_CheckParm("-extratic"))
+    {
         doomcom->extratics = 1;
+    }
     else
+    {
         doomcom->extratics = 0;
+    }
 
     p = M_CheckParm("-port");
     if (p && p < myargc - 1)
     {
-        DOOMPORT = atoi(myargv[p + 1]);
+        // DOOMPORT = atoi(myargv[p + 1]);
+        DOOMPORT = static_cast<short unsigned int>(atoi(myargv[p + 1]));
         printf("using alternate port %i\n", DOOMPORT);
     }
 
@@ -296,8 +315,11 @@ void I_InitNetwork(void)
         {
             hostentry = gethostbyname(myargv[i]);
             if (!hostentry)
+            {
                 I_Error("gethostbyname: couldn't find %s", myargv[i]);
-            sendaddress[doomcom->numnodes].sin_addr.s_addr = *(int *)hostentry->h_addr_list[0];
+            }
+            // sendaddress[doomcom->numnodes].sin_addr.s_addr = *(int *)hostentry->h_addr_list[0];
+            sendaddress[doomcom->numnodes].sin_addr.s_addr = reinterpret_cast<in_addr_t>(hostentry->h_addr_list[0]);
         }
         doomcom->numnodes++;
     }
@@ -324,5 +346,7 @@ void I_NetCmd(void)
         netget();
     }
     else
+    {
         I_Error("Bad net cmd: %i\n", doomcom->command);
+    }
 }
